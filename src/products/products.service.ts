4de61488792db55a031bdd00bf377bf921cc86a4
Wilfrido Heredia <wilfrido.heredia@gmail.com>
2025-12-07 18:56:36 -0500

    initial commit

---
+++ src/products/products.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from 'prisma/generated/client';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  onModuleInit() {
+    this.$connect();
+    console.log(`Database connected!`);
+  }
+  create(createProductDto: CreateProductDto) {
+    return 'This action adds a new product';
+  }
+
+  findAll() {
+    return `This action returns all products`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
